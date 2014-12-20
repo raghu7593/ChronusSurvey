9f73a27525d25e4c45a293a383db8fc2bc77a9c3
Raghu Teja <raghutejaparisa@gmail.com>
2014-12-20 17:48:52 +0530

    Adding login with google option

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
+  provider :google_oauth2, "991405859063-hqa2gregbpcbba69g20m910g9kjfc4cu.apps.googleusercontent.com", "U1ORpZYqWAomA6X24w2Uo0vg"
 end
