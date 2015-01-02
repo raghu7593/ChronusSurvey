9375ef75ea17c893dbf557965db062ff5d7918ec
Raghu Teja <raghutejaparisa@gmail.com>
2015-01-02 18:46:09 +0530

    Assets precompile

--- public/assets/application.js
+++ public/assets/application.js
@@ -14503,6 +14503,10 @@ window.Modernizr = (function( window, document, undefined ) {
 
 
 }).call(this);
+(function() {
+
+
+}).call(this);
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
