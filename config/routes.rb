b5e15c23be4a55010a4e25aabd0f2167a90846af
Raghu Teja <raghuteja.parisa@chronus.com>
2015-02-26 21:19:51 +0530

    Adding views for surveys

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ ChronusSurvey::Application.routes.draw do
   get "static_pages/credits"
   get "static_pages/pricing"
   resources :surveys, :path => 'surveys'
-  resources :surveys, :path => 'surveys', :only => [] do
-  	resources :question_groups, :path => 'question_groups'
-  end
   root to: "sessions#new"
   match "/auth/google_oauth2/callback", to: "sessions#create"
   match "/auth/failure", to: "sessions#failure"
