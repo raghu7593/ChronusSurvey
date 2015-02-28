2718fed461dc90d14cbbf9bcc3b6adbae9d77b31
Raghu Teja <raghuteja.parisa@chronus.com>
2015-02-28 17:35:45 +0530

    Survey form validation

--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -1,8 +1,22 @@
-var Meetings = {
-
+var Surveys = {
   validateSurveyForm: function(){
+    var survey_title = $("#survey_title")
+    survey_title.change(function () {
+      if(survey_title.val() == "") {
+        survey_title.addClass("input-error");
+      }
+      else {
+        survey_title.removeClass("input-error");
+      }
+    });
     $(".survey-form").on('submit', function (event) {
-      
+      if(survey_title.val() == "") {
+        survey_title.addClass("input-error");
+        return false;
+      }
+      else {
+        return true;
+      }
     });
   }
 }
