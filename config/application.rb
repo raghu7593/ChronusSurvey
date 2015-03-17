2cd427d60166ff7f2541a8b19861fd6f09121ace
Raghu Teja <raghuteja.parisa@chronus.com>
2015-03-17 14:58:33 +0530

    Updating files

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,6 @@ module ChronusSurvey
 
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
-    config.assets.precompile = %w( files )
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
   end
 end
