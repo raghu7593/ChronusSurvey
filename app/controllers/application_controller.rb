82bf9f09137d62f464e80f65b971754f1c5333c2
Raghu Teja <raghuteja.parisa@chronus.com>
2015-04-11 18:08:27 +0530

    Login with Google and some CSS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  skip_before_action :verify_authenticity_token, if: :js_request?
 
-private
+  protected
+  def js_request?
+    request.format.js?
+  end
 
+  private
   def current_user
     @current_user ||= Member.find_by_id(session[:member_id]) if session[:member_id]
   end
